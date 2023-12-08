592e5298cf370a08b8664c2633778b20a31e4526
nkmr <tomnkmr0120@gmail.com>
2023-12-08 16:56:37 +0900

    ログインの実装

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,8 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    //コンテナ外からのアクセスに対応
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
